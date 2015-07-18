7a4d0274d311700682e07dd0ac50313e9c85cdca
Charles Daniels <cdaniels@fastmail.com>
2015-07-18 17:46:50 -0400

    sideman has been retired, starting over...

--- bin/genpw.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# generate a password of five random words
-shuf --random-source=/dev/urandom -n 5 $HOME/lib/words | tr '\n' ' ';echo""
--- bin/macos-browser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# cuses open with browser within a linux VM to open the browser on the mac OS host
-ssh cdaniels@192.168.56.1 "/Users/cdaniels/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome $1"
