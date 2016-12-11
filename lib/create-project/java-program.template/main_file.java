a72c5e8f9e0631a0484637251b8c12923169e6c3
Charles Daniels <cdaniels@fastmail.com>
2016-12-11 17:02:04 -0500

    moved create-project to toolchest

--- lib/create-project/java-program.template/main_file.java
+++
@@ -1,9 +0,0 @@
-
-  public static void main(String[] args) {
-    System.out.println("INFO: beginning program execution");
-
-    // YOUR CODE HERE
-
-    System.out.println("INFO: halting program execution");
-  }
-}
