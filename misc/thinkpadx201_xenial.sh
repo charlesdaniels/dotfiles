7c412d33e1dbde3fcd21ef0b4afc463bc57d1a06
Charles Daniels <cdaniels@fastail.com>
2018-01-16 17:01:29 -0500

    added thinkpad x201 script for Linux/xenial

---
+++ misc/thinkpadx201_xenial.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# .SCRIPTDOC
+
+# Performs provisioning steps specific to running Linux/xenial on the Thinkpad
+# X201, most importantly configuring thinkfan.
+
+# .ENDOC
+
+SCRIPTDIR="$(dirname "$0")"
+sudo apt install --yes thinkfan
+sudo echo "thinkpad_acpi experimental=1 fan_control=1" >> /etc/modprobe.d/thinkpad.conf
+sudo systemctl enable thinkfan.service
+sudo cp "$SCRIPTDIR/thinkfan.conf" "/etc/thinkfan.conf"
