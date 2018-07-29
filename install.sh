b6e2f390f81850eaab1b089d6e57be213ea26ca1
Charles Daniels <cdaniels@fastmail.com>
2018-07-28 23:26:12 -0400

    added shell extensions

--- install.sh
+++ install.sh
@@ -18,3 +18,52 @@ find . -type f | while read -r target ; do
 	mkdir -p "$dest_parent"
 	cp "$target" "$dest_file"
 done
+
+# install shell extensions
+
+# ensure the directory exists
+ZSH_DIR="$HOME/.zsh"
+if [ ! -d "$ZSH_DIR" ] ; then
+	mkdir -p "$ZSH_DIR"
+fi
+if [ ! -d "$ZSH_DIR" ] ; then
+	echo "FATAL: could not create $ZSH_DIR"
+	exit 1
+fi
+cd "$ZSH_DIR"
+
+# zsh fast syntax highlighting
+if [ ! -d "./fast-syntax-highlighting" ] ; then
+	git clone --quiet git://github.com/zdharma/fast-syntax-highlighting.git
+fi
+cd fast-syntax-highlighting
+git reset --hard HEAD --quiet
+git pull --quiet origin master
+cd "$ZSH_DIR"
+
+# zsh-autosuggestions
+if [ ! -d "./zsh-autosuggestions" ] ; then
+	git clone --quiet git://github.com/zsh-users/zsh-autosuggestions.git
+fi
+cd zsh-autosuggestions
+git reset --hard HEAD --quiet
+git pull --quiet origin master
+cd "$ZSH_DIR"
+
+# zsh-abbr-path
+if [ ! -d "./zsh-abbr-path" ] ; then
+	git clone --quiet git://github.com/felixgravila/zsh-abbr-path.git
+fi
+cd zsh-abbr-path
+git reset --hard HEAD --quiet
+git pull --quiet origin master
+cd "$ZSH_DIR"
+rm -f ./abbr_pwd.zsh
+ln -s ./zsh-abbr-path/.abbr_pwd ./abbr_pwd.zsh
+
+# git completions
+rm -f "$HOME/.git-completion.bash"
+rm -f "$HOME/.git-completion.zsh"
+BASE_URL='https://raw.githubusercontent.com/git/git/master/contrib/completion'
+curl -LSs "$BASE_URL/git-completion.bash" -o "$HOME/.git-completion.bash"
+curl -LSs "$BASE_URL/git-completion.zsh" -o "$HOME/.git-completion.zsh"
